973fef2ee1b6a3f4afc0a0fc7464a0b057c0e0ec
Hugo Santos <hugosdsantos@hotmail.com>
2015-04-16 14:12:07 +0100

    Add opened and closed dates for statistics

--- project_task_statistic/project_task_sla_close.py
+++ project_task_statistic/project_task_sla_close.py
@@ -5,26 +5,34 @@ from datetime import datetime, timedelta
 class project_task_close_action(models.Model):
     _inherit = "project.task"
 
-    date_closed = fields.Datetime(
-        'Date Closed', compute='_close_task_date', store=True)
+    date_opened = fields.Datetime('Date Opened')
+    date_closed = fields.Datetime('Date Closed')
+
+    @api.model
+    def create(self, vals):
+        if not (self.date_opened) and self.stage_id.state == 'open':
+            vals['date_opened'] = fields.datetime.now().strftime(
+                '%Y-%m-%d %H:%M:%S')
+
+        elif self.stage_id.state in ('cancelled', 'done'):
+            vals['date_closed'] = fields.datetime.now().strftime(
+                '%Y-%m-%d %H:%M:%S')
+        else:
+            vals['date_closed'] = None
+        res = super(project_task_close_action, self).create(vals)
+        return res
 
     @api.one
-    @api.depends('stage_id')
-    def _close_task_date(self):
-        if self.work_ids and self.stage_id.state in ('cancelled','done'):
-            #The list is by default already order by desc
-            last_line = self.work_ids[0]
+    def write(self, vals):
+        if not (self.date_opened) and self.stage_id.state == 'open':
+            vals['date_opened'] = fields.datetime.now().strftime(
+                '%Y-%m-%d %H:%M:%S')
 
-            #Get the hours and date
-            rec_hours = last_line.hours
-            rec_max_date = last_line.date
-
-            #Adding to the max workline date the duration time
-            sla_date_close = datetime.strptime(
-                rec_max_date, '%Y-%m-%d %H:%M:%S')
-            sla_date_close += timedelta(hours=rec_hours)
-
-            #Now finaly update date_closed field
-            self.date_closed = sla_date_close
+        elif self.stage_id.state in ('cancelled', 'done'):
+            if not(self.date_closed):
+                vals['date_closed'] = fields.datetime.now(
+                    ).strftime('%Y-%m-%d %H:%M:%S')
         else:
-            self.date_closed = None
+            vals['date_closed'] = None
+        res = super(project_task_close_action, self).write(vals)
+        return res
