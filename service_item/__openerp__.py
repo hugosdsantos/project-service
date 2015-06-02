b727a7c5ba6e946ed0e10e8db4bdca08fef11f44
Hugo Santos <hugosdsantos@hotmail.com>
2015-06-02 08:56:36 +0100

    Add Service Item module

---
+++ service_item/__openerp__.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2012-2013 Daniel Reis
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+    'name': 'Service Item',
+    'summary': 'Use Projects for Service Desks and service teams',
+    'version': '1.1',
+    "category": "Project Management",
+    'description': """\
+
+""",
+    'author': 'Daniel Reis',
+    'website': '',
+    'depends': [
+        'analytic','hr'
+    ],
+    'data': [
+        'service_item.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
---
+++ service_item/service_item.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) 2015 Daniel Reis
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import models, fields, api
+
+
+class ServiceItem(models.Model):
+    _name = 'service.item'
+    _description = "Service Item"
+
+    #service_item_id = fields.Char("Service Item ID")
+    name = fields.Char('Name')
+    code = fields.Char('Code')
+    site_id = fields.Many2one(string='Site', comodel_name='res.partner')
+    state_id = fields.Selection(string='State', selection=[('draft', 'Draft'),('active', 'Active'),('close', 'Closed')])
+    date_instalation = fields.Date('Date Instalation')
+
+    analytic_account_id = fields.Many2one(string='Analytic Account', comodel_name='account.analytic.account')
+    department_id = fields.Many2one(string='Department', related='analytic_account_id.department_id')
+    partner_id = fields.Many2one(string='Customer', related='analytic_account_id.partner_id')
+    
+    item_group_id = fields.Many2one(string="Item Group", comodel_name='service.item.group')
+    contract_group_id = fields.Many2one(string="Contract Group", comodel_name='service.contract.group')
+
+
+class ServiceItemGroup(models.Model):
+    _name = 'service.item.group'
+    _description = "Service Item Group"
+
+    name = fields.Char('Name')
+    description = fields.Char('Description')
+
+
+class ServiceContractGroup(models.Model):
+    _name = 'service.contract.group'
+    _description = "Service Contract Group"
+
+    name = fields.Char('Name')
+    description = fields.Char('Description')
+
